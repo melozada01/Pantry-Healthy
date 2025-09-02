2764eff9476eedc7a9854db7c795ad965dd35638
Gabriel Mota <gabrielmotaproo@gmail.com>
2025-09-01 21:32:28 -0300

    Delete telas de cadastro directory

--- telas de cadastro/javascript/java.js
+++
@@ -1,7 +0,0 @@
-function startApp() {
-    alert("Vamos começar! Redirecionando...");
-    // Aqui você pode colocar uma navegação, por exemplo:
-    // window.location.href = "requisitos.html";
-  }
-
-  
