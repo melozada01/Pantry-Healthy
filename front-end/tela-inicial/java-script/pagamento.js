a1b6daae4d7b45401f5897f61784cc47df826856
Gabriel Mota <gabrielmotaproo@gmail.com>
2025-09-01 21:31:59 -0300

    Delete front-end directory

--- front-end/tela-inicial/java-script/pagamento.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.pay-btn').addEventListener('click', () => {
-  alert('Pagamento realizado com sucesso!');
-  window.location.href = 'dashboard.html'; // volta pro dashboard, se quiser
-});
